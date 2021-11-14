493025d34cba7074fa5ff84c88856449228d0762
Daniel6996-arch <nyutu.daniel@student.moringaschool.com>
2021-11-14 22:23:01 +0300

    add changes to models

--- app/models.py
+++ app/models.py
@@ -4,6 +4,10 @@ from flask_login import UserMixin
 from . import login_manager
 
 
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
 class User(UserMixin,db.Model):
     __tablename__ = 'users'
 
@@ -14,7 +18,6 @@ class User(UserMixin,db.Model):
     bio = db.Column(db.String(255))
     profile_pic_path = db.Column(db.String())
     password_secure = db.Column(db.String(255))
-
 
     @property
     def password(self):
